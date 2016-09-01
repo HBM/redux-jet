985db7b6cc30dd2bf797115291044ac453ea15a4
Gerhard Preuss <gelipp@gmail.com>
2016-09-01 10:37:47 +0200

    add more test

--- test/actions.spec.js
+++ test/actions.spec.js
@@ -14,21 +14,49 @@ describe('actions', () => {
     return peer.connect()
   })
 
-  it('connect', (done) => {
-    let i = 0
-    connect({url: 'ws://localhost:11123'})((action) => {
-      if (i === 0) {
-        assert.deepEqual(action, {
-          type: 'JET_CONNECT_REQUEST',
-          url: 'ws://localhost:11123',
-          password: undefined,
-          user: undefined
-        })
-        ++i
-      } else {
-        console.log(action)
-        done()
-      }
+  describe('connect', () => {
+    it('ok', (done) => {
+      let i = 0
+      connect({url: 'ws://localhost:11123'})((action) => {
+        if (i === 0) {
+          assert.deepEqual(action, {
+            type: 'JET_CONNECT_REQUEST',
+            url: 'ws://localhost:11123',
+            password: undefined,
+            user: undefined
+          })
+          ++i
+        } else {
+          assert.deepEqual(action, {
+            type: 'JET_CONNECT_SUCCESS',
+            url: 'ws://localhost:11123',
+            password: undefined,
+            user: undefined
+          })
+          done()
+        }
+      })
+    })
+
+    it('fail', (done) => {
+      let i = 0
+      connect({url: 'ws://foo.bar:11123'})((action) => {
+        if (i === 0) {
+          assert.deepEqual(action, {
+            type: 'JET_CONNECT_REQUEST',
+            url: 'ws://foo.bar:11123',
+            password: undefined,
+            user: undefined
+          })
+          ++i
+        } else {
+          const {type, url, error} = action
+          assert.equal(type, 'JET_CONNECT_FAILURE')
+          assert.equal(url, 'ws://foo.bar:11123')
+          assert(error)
+          done()
+        }
+      })
     })
   })
 })
